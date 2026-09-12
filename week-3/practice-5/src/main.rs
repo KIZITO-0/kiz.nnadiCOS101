1bd8a9a0f350627771d157c89e2a19866458ccc2
Nnadi Kizito <kizito.nnadi@pau.edu.ng>
2026-09-12 22:10:16 +0100

    (week 3 practice)

---
+++ week-3/practice-5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'B';
+    let surname = "Kizito";
+    let first_name = "ifeoma";
+    let middle_name = "silvia";
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+}
---
+++ week-3/practice-5/target/debug/practice-5.d
@@ -0,0 +1,1 @@
+C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-3\practice-5\target\debug\practice-5.exe: C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-3\practice-5\src\main.rs
