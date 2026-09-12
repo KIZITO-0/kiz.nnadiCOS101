af59500bcf304375d1634107ce293da04ad10ca8
Nnadi Kizito <kizito.nnadi@pau.edu.ng>
2026-09-12 20:04:25 +0100

    (Week 3 practice)

---
+++ week-3/practice-2/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let result = 60;      // i32 by default
+    let age:u32 = 560;
+    let sum:i32 = 9 - 80;
+
+    println!("Result value is {}", result);
+    println!("Age is {}", age);
+    println!("Sum is {}", sum);
+}
---
+++ week-3/practice-2/target/debug/practice-2.d
@@ -0,0 +1,1 @@
+C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-3\practice-2\target\debug\practice-2.exe: C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-3\practice-2\src\main.rs
