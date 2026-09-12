8e148aa535439bc715bbb5b690448b20ef7a4b84
Nnadi Kizito <kizito.nnadi@pau.edu.ng>
2026-09-12 22:33:59 +0100

    (week 3 project)

---
+++ week-3/practice-8/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let fees = 29_000;
+    println!("fees is {} ",fees);
+
+   let fees = 30_000;
+    println!("fees changed is {}",fees);
+}
+
---
+++ week-3/practice-8/target/debug/practice-8.d
@@ -0,0 +1,1 @@
+C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-3\practice-8\target\debug\practice-8.exe: C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-3\practice-8\src\main.rs
