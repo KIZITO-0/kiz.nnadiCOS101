da633d0ddd1bad7d0418f763b01594cdfc8c31d2
Nnadi Kizito <kizito.nnadi@pau.edu.ng>
2026-09-17 17:24:45 +0100

    (week 4 practice)

---
+++ week-4/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-4\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-4\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-4\practice_3\target\debug\practice_3.exe: C:\Users\kizito\Documents\COS101\kiz.nnadiCOS101\week-4\practice_3\src\main.rs
